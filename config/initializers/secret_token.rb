ec6d95f2babab0754e1e1835a48409e0010d01f3
Christiano Milfont <cmilfont@gmail.com>
2012-02-25 03:06:40 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cordel::Application.config.secret_token = 'c7a61fe4fe6ba9b89d87ea26a334096a64789059e321c6dda1ac7203fb7780456b51d241765e2c06a1a5c73908b2ed87102855c01dace694476c0b29e222be3e'
